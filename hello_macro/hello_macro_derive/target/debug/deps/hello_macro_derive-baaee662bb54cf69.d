a5c5d35a89407a51f66473238fe7d3d96d2c4a6a
aniruddh-alt <77236089+aniruddh-alt@users.noreply.github.com>
2024-12-27 12:09:46 +0400

    Remove target directories from Git tracking

---
+++ hello_macro/hello_macro_derive/target/debug/deps/hello_macro_derive-baaee662bb54cf69.d
@@ -0,0 +1,5 @@
+C:\Users\aniru\Desktop\Programming Languages Projects\Searchly\hello_macro\hello_macro_derive\target\debug\deps\hello_macro_derive-baaee662bb54cf69.dll: src\lib.rs
+
+C:\Users\aniru\Desktop\Programming Languages Projects\Searchly\hello_macro\hello_macro_derive\target\debug\deps\hello_macro_derive-baaee662bb54cf69.d: src\lib.rs
+
+src\lib.rs:
